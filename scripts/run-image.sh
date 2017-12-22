6d4020dc4e33e30f9709570d200cf7472dc6f4ce
Mikhail Nasyrov <streambot@gmail.com>
2017-12-22 13:04:21 +0700

    Moved 3.3.0.Final image to the stable branch.

--- scripts/run-image.sh
+++ scripts/run-image.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CERTS_DIR=${CERTS_DIR:-"${SCRIPT_DIR}/certs"}
+CERTS_DIR="${SCRIPT_DIR}/certs"
 
 IMAGE_NAME="${1:-keycloak-postgres-ssl}"
 
-docker run --rm --name keycloak-postgres-ssl -d -p 9080:8080 \
+docker run --rm --name keycloak-postgres-ssl -p 9080:8080 \
   --link postgres-ssl:postgres \
-  -e KEYCLOAK_POSTGRES_HOST=postgres \
-  -e KEYCLOAK_POSTGRES_DATABASE=keycloak-db \
-  -e KEYCLOAK_POSTGRES_USER=keycloak-user \
-  -e KEYCLOAK_POSTGRES_PASSWORD=keycloak \
+  -e KEYCLOAK_USER=admin \
+  -e KEYCLOAK_PASSWORD=admin \
+  -e POSTGRES_PORT_5432_TCP_ADDR=postgres \
+  -e POSTGRES_DATABASE=keycloak-db \
+  -e POSTGRES_USER=keycloak-user \
+  -e POSTGRES_PASSWORD=keycloak \
   -e KEYCLOAK_JDBC_PARAMS='sslmode=verify-ca' \
-  -v ${CERTS_DIR}/keycloak-certs.jks:/opt/jboss/keycloak-certs.jks \
-  ${IMAGE_NAME} \
-  -b=0.0.0.0 \
-  -Djavax.net.ssl.trustStore=/opt/jboss/keycloak-certs.jks \
-  -Djavax.net.ssl.trustStorePassword=password
+  -e POSTGRES_JDBC_PARAMS='sslrootcert=/opt/jboss/postgres.crt.der' \
+  -v ${CERTS_DIR}/postgres.crt.der:/opt/jboss/postgres.crt.der \
+  ${IMAGE_NAME}
